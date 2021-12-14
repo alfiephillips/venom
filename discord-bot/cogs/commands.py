6a9dc49697a111c09b80c1633219bab9f5e16c11
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-12-14 23:03:07 +0000

    discord bot updates

--- discord-bot/cogs/commands.py
+++ discord-bot/cogs/commands.py
@@ -4,6 +4,7 @@ import discord
 
 from discord.ext import commands
 
+
 class Commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -13,10 +14,9 @@ class Commands(commands.Cog):
         embed = discord.Embed(title=title, description=description)
         return embed
 
-    @commands.command(name="ping", help="Test the bots current ping!")
-    async def ping(self, ctx):
-        embed = self.em(title="My current ping!", description=f"{round(self.bot.latency * 1000, 1)}ms")
-        return await ctx.send(embed=embed)
+    @commands.command(name="newlink", help="Create a new link to scalp on either Amazon, ...")
+    async def new_link(self, ctx, link):
+        pass
 
 def setup(bot):
     bot.add_cog(Commands(bot))
